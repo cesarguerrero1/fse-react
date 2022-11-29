0c7c9a1b4cef5bd4851baf521a59ffa0d261f854
Cesar Guerrero <cguerrero.cesar@gmail.com>
2022-11-29 13:54:16 -0500

    Refactored the likes service to now handle both likes and dislikes. ADditionally updated TUits to now show both the likes and dislikes

--- src/components/tuits/index.js
+++ src/components/tuits/index.js
@@ -1,14 +1,18 @@
 import React from "react";
 import './tuits.css';
 import Tuit from "./tuit";
-import * as likesService from "../../services/likes-service.js";
+import * as likesDislikesService from "../../services/likes-dislikes-service.js";
 
 function Tuits({ tuits = [], deleteTuit, refreshTuits }) {
 
   //Essentially what we are doing is handling a like event and then checking it if is a 'like' or 'dislike' event
   const handleLikeEvent = async(tuit, eventType) => {
       try{
-        await likesService.userTogglesLikeEvent("me", tuit._id, eventType);
+        if(eventType === "likes"){
+          await likesDislikesService.userTogglesLikeEvent("me", tuit._id);
+        }else if(eventType === "dislikes"){
+          await likesDislikesService.userTogglesDislikeEvent("me", tuit._id);
+        }
         refreshTuits();
       }catch(error){
         alert("You are not allowed to like or dislike a Tuit if you are not logged in!");
